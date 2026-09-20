0f8a098185fbdcdb810c7c0f10c28855d1358efe
akshayjango <vakshay083@gmail.com>
2026-09-20 11:01:25 +0530

    fix(ui): restore clean white system navigation bar by reverting to FlutterActivity

--- dealbuster_app/android/app/src/main/kotlin/com/dealbusterindia/app/MainActivity.kt
+++ dealbuster_app/android/app/src/main/kotlin/com/dealbusterindia/app/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.dealbusterindia.app
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
-        super.onCreate(savedInstanceState)
-    }
+class MainActivity: FlutterActivity() {
 }
+
 
 
